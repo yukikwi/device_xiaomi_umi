b9bd84275c486c9c231430be2d0ca3992b239be1
Pachara <pachara2202@gmail.com>
2022-04-15 15:15:15 +0700

    Revert "umi: Import Pixel thermal HAL"
    This reverts commit b3a4e51c66307123f02bebe7287c806da64cdb11.

--- device.mk
+++ device.mk
@@ -359,9 +359,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal HAL
-    android.hardware.thermal@2.0-service.pixel
+# Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_PACKAGES += \
