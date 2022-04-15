b3a4e51c66307123f02bebe7287c806da64cdb11
Pachara <pachara2202@gmail.com>
2022-04-15 15:15:15 +0700

    umi: Import Pixel thermal HAL
    https://github.com/LineageOS/android_device_xiaomi_sm6250-common

--- device.mk
+++ device.mk
@@ -359,9 +359,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
+# Thermal HAL
+    android.hardware.thermal@2.0-service.pixel
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_PACKAGES += \
