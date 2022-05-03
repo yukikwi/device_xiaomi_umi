424ecb8492db6b40f6e246659ec34474df4b5bf3
Pachara <pachara2202@gmail.com>
2022-05-03 23:07:18 +0700

    Revert "umi: Sync with 13.0.5.1"
    This reverts commit 1e78287a64301b2ad1824834367739e5650f06a8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,4 +213,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
-include vendor/xiaomi/umi/BoardConfigVendor.mk
+include vendor/xiaomi/umi_miui13/BoardConfigVendor.mk
